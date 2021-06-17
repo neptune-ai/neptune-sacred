9469742f0131497b750bd55c5adee739e55578f4
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-06-17 18:18:33 +0200

    Fix runtime version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
-from neptune_sacred._version import get_versions
+import versioneer
 
 
 def main():
@@ -26,7 +26,7 @@ def main():
             if line.startswith('Version:'):
                 version = line[8:].strip()
     else:
-        version = get_versions()["version"]
+        version = versioneer.get_version()
 
     setup(
         name='neptune-sacred',
@@ -47,7 +47,8 @@ def main():
         license='Apache License 2.0',
         install_requires=base_libs,
         extras_require=extras,
-        packages=['neptune_sacred', 'neptune_sacred.impl'],
+        packages=find_packages(),
+        cmdclass=versioneer.get_cmdclass(),
         zip_safe=False,
         classifiers=[
             # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
