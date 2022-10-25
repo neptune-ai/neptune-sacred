9f231c4b64632d24817716c8ff0e315fdf64f41d
Rafał Jankowski <rafal.jankowski@neptune.ai>
2022-10-25 15:32:09 +0200

    Added flake8 to pre-commit (#18)

--- src/neptune_sacred/__init__.py
+++ src/neptune_sacred/__init__.py
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+__all__ = [
+    "NeptuneObserver",
+    "__version__",
+]
 
 from neptune_sacred.impl import (
     NeptuneObserver,
