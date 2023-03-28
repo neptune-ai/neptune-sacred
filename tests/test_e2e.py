15e60716675c53543d01d4ebb2eb1acfd3be6596
AleksanderWWW <alwojnarowicz@gmail.com>
2023-03-28 12:12:01 +0200

    Update integration after recent deprecations (#20)
    * Update integration after recent deprecations
    
    * bump isort
    
    * update changelog
    
    * docstring adjustments
    
    - Update links
    - Format code examples
    - Phrasing and spelling
    
    * explicitly cast _id and host_info to strings
    
    * add tests
    
    * add tests to ci
    
    * change example run link
    
    * correct action name
    
    * update link - now hardware metrics are logged
    
    * put neptune imports in try-except block
    
    * update requirement for neptune
    
    * try with nonexistent package
    
    * revert to previous version
    
    * remove python 3.6 from target versions
    
    * update
    
    * File instead of object in elif condition
    
    * simplify imports
    
    * fix mistake with repo name
    
    * apply code review suggestions
    
    * restore expect_not_an_experiment
    
    * add torch to dev requirements
    
    * add torchvision to dev requirements
    
    * loosen torchvision version specification
    
    * add scikit-learn to dev reqs to make doctest happy
    
    * make doctest happy
    
    * remove doctest from ci action
    
    * Update NeptuneObserver docstring (#23)
    
    * update NeptuneObserver docstring
    
    * Update .pre-commit-config.yaml
    
    * apply pre-commit
    
    * move neptune from base to optional dev requirements
    
    * imports with backward compatibility and installation checking at runtime
    
    * update changelog
    
    * simplify import of Run
    
    * apply code review suggestions
    
    ---------
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>

---
+++ tests/test_e2e.py
@@ -0,0 +1,47 @@
+import neptune
+import torch
+
+from neptune_sacred import NeptuneObserver
+
+
+def test_e2e(experiment, model):
+
+    # Step 1: Initialize Neptune and create new Neptune Handler
+    neptune_handler = neptune.init_run()["sacred"]
+
+    # Step 2: Add NeptuneObserver() to your sacred experiment's observers
+    experiment.observers.append(NeptuneObserver(run=neptune_handler))
+
+    # Step 3: Run you experiment and explore metadata in Neptune UI
+    experiment.run()
+
+    # More Options
+    # Step 4: Log Artifacts (Model architecture and weights)
+
+    # Save model architecture
+    model_fname = "model"
+    with open(f"{model_fname}_arch.txt", "w") as f:
+        f.write(str(model))
+
+    # Save model weights
+    torch.save(model.state_dict(), f"./{model_fname}.pth")
+
+    # Log model architecture and weights
+    experiment.add_artifact(name=model_fname + "_arch", filename=f"./{model_fname}_arch.txt")
+    experiment.add_artifact(name=model_fname, filename=f"./{model_fname}.pth")
+
+    run = neptune_handler.get_root_object()
+    run.sync()
+
+    assert run["sacred/experiment/config/data_dir"].fetch() == "data/CIFAR10"
+
+    assert run.exists("sacred/experiment/io_files/artifacts/model")
+    assert run.exists("sacred/experiment/io_files/artifacts/model_arch")
+
+    assert 0 <= run["sacred/experiment/metrics/results/final_acc"].fetch() <= 1
+
+    assert run.exists("sacred/experiment/sacred_config/experiment_info")
+    assert run.exists("sacred/experiment/sacred_config/meta_info")
+
+    # Stop run
+    run.stop()
